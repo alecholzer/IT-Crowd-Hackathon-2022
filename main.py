4b412a1e302f8bc1e5275f31dbda8dd39d5c0226
Lodigolden <adamhagengruber@gmail.com>
2022-02-19 20:31:21 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ class Sprite(pygame.sprite.Sprite):
     def __init__(self,pos):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface([20,20])
+        self.image.fill((0,0,255))
+        self.rect = self.image.get_rect()
+        self.rect.center = pos
+
+# Initialize ground class:
+class Ground(pygame.sprite.Sprite):
+    def __init__(self,pos):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface([300,20])
         self.image.fill((255,0,255))
         self.rect = self.image.get_rect()
         self.rect.center = pos
@@ -22,41 +31,47 @@ def main():
     bg = [0,0,0]
     size = [300,300]
     screen = pygame.display.set_mode(size)
-    player = Sprite([40,50])
+
+    #Logic variables:
     jump = False
+    gravity = 1
+    onGround = False
 
-    # Set move values:
-    player.move = [pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN]
-    player.vx = 5
-    player.vy = 5
-
+    # Player initialization:
+    player = Sprite([40,250])
+    player_group = pygame.sprite.Group()
+    player_group.add(player)
     previousx = player.rect.x
     previousy = player.rect.y
-    wall = Sprite([100,60])
+    player.vy = 5
+    player.vx = 5
 
+    # Wall initialization:
+    wall = Sprite([100,60])
     wall_group = pygame.sprite.Group()
     wall_group.add(wall)
 
-    player_group = pygame.sprite.Group()
-    player_group.add(player)
+    # Ground initialization:
+    ground = Ground([150,290])
+    ground_group = pygame.sprite.Group()
+    ground_group.add(ground)
 
     while True:
+        # Set loop end:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return False
 
-        # Get keyboard input from user:
+        # Get keyboard input:
         key = pygame.key.get_pressed()
-
-        # Move logic:
         if key[pygame.K_LEFT]:
             player.rect.x -= 1
         if key[pygame.K_RIGHT]:
             player.rect.x += 1
-        #if key[pygame.K_UP]:
-        #    player.rect.y -= 1
-        #if key[pygame.K_DOWN]:
-        #   player.rect.y += 1
+
+        # Gravity logic:
+        if onGround == False:
+            player.rect.y += 1
 
         # Jump logic:
         if jump == False and key[pygame.K_SPACE]:
@@ -65,32 +80,38 @@ def main():
         if jump == True:
             player.rect.y -= player.vy
             player.vy -= 1
+            onGround = True
             if player.vy < -5:
                 jump = False
                 player.vy = 5
 
-        # Hit logic:
-        hit = pygame.sprite.spritecollide(player, wall_group, False)
-        if hit:
-            player.image.fill((255,255,255))
+        # Hit commands:
+        hitGround = pygame.sprite.spritecollide(player, ground_group, False)
+        hitWall = pygame.sprite.spritecollide(player, wall_group, False)
+
+        # Check ground:
+        if hitGround:
+            onGround = True
+        else:
+            onGround = False
+
+        # Check wall:
+        if hitWall:
             player.rect.x = previousx
             player.rect.y = previousy
         else:
-            if player.image.fill != (255,0,255):
-                player.image.fill((255,0,255))
             previousx = player.rect.x
             previousy = player.rect.y
 
-        # Delay:
-        pygame.time.delay(50)
+        # Output to display:
+        screen.fill(bg)
         player_group.draw(screen)
         wall_group.draw(screen)
         pygame.display.update()
         clock.tick(fps)
-        screen.fill(bg)
 
+    # Exit game:
     pygame.quit()
     sys.exit
 
-# Call main:
 main()
